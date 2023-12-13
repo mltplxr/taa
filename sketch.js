20faf0f7f211b117d5f3120d215b351df9874185
mltplxr <128910989+mltplxr@users.noreply.github.com>
2023-12-13 10:10:20 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let nDots = 60; //70
 let nLines = 20; //10
 let coupling = 0;
 let spaceWidth = 2;
-let gapRandomness = 0;
+let globalGapProbability = 0.2; // Adjust this value for gap prevalence
 let lineWidth = 0.5;
 let initialLineWidth = 0.5;
 
@@ -17,32 +17,36 @@ function draw() {
   background(255);
 
   for (let i = 2; i < nLines; i++) {
-    let y = map(i, 0, nLines - 1, 0, height -50);
-  
-    beginShape();
-    
-  
+    let y = map(i, 0, nLines - 1, 0, height - 50);
 
-   for (let j = 0; j < nDots; j++) {
+    beginShape();
+
+    for (let j = 0; j < nDots; j++) {
       let x = map(j, 0, nDots - 1, 10, width - 10);
       let xDelta = random(-xDisplace, xDisplace);
       let yDelta = (0.3 - coupling) * random(-yDisplace, yDisplace); //1
 
       yDelta -= coupling * xDelta * (yDisplace / xDisplace);
 
-      if (random() < gapRandomness && j / nDots < gapRandomness) {
+      if (random() < globalGapProbability) {
         // Introduce a gap
+        endShape(); // End the current shape
+        beginShape(); // Start a new shape
         move(x + xDelta, y + yDelta);
-      }       else {
+      } else {
         // Continue the line
+        strokeWeight(map(j, 0, nDots - 1, initialLineWidth, lineWidth));
         curveVertex(x + xDelta, y + yDelta);
       }
     }
 
     endShape();
     noLoop();
-    
+  }
 }
+
+function move(x, y) {
+  vertex(x, y);
 }
 
 function mousePressed() {
@@ -51,6 +55,15 @@ function mousePressed() {
   redraw(); // Update the state and redraw
 }
 
+
+
+
+
+function mousePressed() {
+  xDisplace = mouseX / width * 100;
+  yDisplace = mouseY / height * 100;
+  redraw(); // Update the state and redraw
+}
 
 
 function keyPressed() {
